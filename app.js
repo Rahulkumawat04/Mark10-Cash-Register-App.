5a4e6e75b82832cf24383f8f1c53ef2e92901968
Rahul Kumawat <mailrahul2004@gmail.com>
2021-08-03 20:30:46 +0530

    JS fixed

--- app.js
+++ app.js
@@ -2,38 +2,34 @@ var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-given");
 var checkButton = document.querySelector("#check-button");
 var message = document.querySelector("#error-message");
-var NoOfNotes = document.querySelectorAll(".no-of-notes")
+var noOfnotes = document.querySelectorAll(".no-of-notes")
 
 var availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function validateBillAndCashAmount(){
-    message.style.display = "none";
-    if(billAmount.value > 0 && cashGiven.value > billAmount.value){
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    if(billAmount.value > 0){
         if(cashGiven.value > billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+            var amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
         }
-        else{
-            showMessage("The Given Cash should be greater than or equal to the Bill Amount")
-        }
+       else{
+           console.log("give more cash");
+       }
     }
     else{
-        showMessage("Invalid Bill Amount");
+        console.log("Enter a POSITIVE Bill Amount.")
     }
+    
 });
 
 function calculateChange(amountToBeReturned){
-
     for(let i=0; i < availableNotes.length; i++){
-        var NoOfNotes = Math.trunc(
-            amountToBeReturned/availableNotes[i]
+        var numberOfnotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
         );
+
         amountToBeReturned %= availableNotes[i];
-        NoOfNotes[i].innerHTML = NoOfNotes;   
+
+        noOfnotes[i].innerHTML = numberOfnotes; 
     }
-}
-function showMessage(msg){
-    message.style.display="block";
-    message.innerHTML = msg;
-    // alert("invalid cash given.");
 }
