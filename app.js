bb7719792c45f3594c55ea4c87ec2e7a67064d57
Rahul Kumawat <mailrahul2004@gmail.com>
2021-08-03 22:01:08 +0530

    app finished. animation to be added.

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-given");
 var checkButton = document.querySelector("#check-button");
 var message = document.querySelector("#error-message");
-var noOfnotes = document.querySelectorAll(".no-of-notes")
+var noOfnotes = document.querySelectorAll(".no-of-notes");
 
-var availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+var returnChange = document.querySelector("#returnAmount");
+
+var availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
     if(billAmount.value > 0){
         if(cashGiven.value > billAmount.value){
             var amountToBeReturned = cashGiven.value - billAmount.value;
+            returnAmount.innerHTML = "The amount to be returned is: ₹" + amountToBeReturned;
             calculateChange(amountToBeReturned);
         }
        else{
-           console.log("give more cash");
+           showMessage("give more cash");
        }
     }
     else{
-        console.log("Enter a POSITIVE Bill Amount.")
+        showMessage("Enter a POSITIVE Bill Amount.")
     }
     
 });
@@ -32,4 +36,18 @@ function calculateChange(amountToBeReturned){
 
         noOfnotes[i].innerHTML = numberOfnotes; 
     }
+}
+
+function hideMessage(){
+    message.style.display = "none";
+}
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.style.backgroundColor = "red";
+    message.style.color = "white";
+    message.style.padding = "1rem";
+    message.style.border = "2px solid white";
+    message.style.borderRadius = "2rem";
+    message.innerHTML = msg;
 }
